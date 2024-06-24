f47223a837785019eb891d4697c6727bc120322a
Gabriel Rosa <gabokuck1990@gmail.com>
2024-06-24 10:55:01 -0600

    se configuran las variables de entorno en el proyecto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // logger
+  const logger = new Logger('Bootstrap');
+
+  // CORS
+  app.enableCors();
+
+  await app.listen(process.env.PORT);
+  logger.log(`App running on port: ${process.env.PORT}`);
 }
 bootstrap();
